32407256e76a4b4c5c76eec90dacc7aec418b456
brunacorreia-gif <brunacorreia@uni9.edu.br>
2025-10-09 08:54:44 -0300

    Atualizar o aula06_01.php

--- aula06_01.php
+++ aula06_01.php
@@ -11,6 +11,6 @@
         Login <input type="text" name="login<br>">
         Senha <input type="password" name="senha<br>">
         <input type="submit" value="Entrar">
-    </form>
+</form>
 </body>
 </html>
