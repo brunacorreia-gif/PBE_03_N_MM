7080ed65f5c5eae2b3f03e90f53744c5e5fda522
brunacorreia-gif <brunacorreia@uni9.edu.br>
2025-10-09 08:46:19 -0300

    Atualizar o aula06_01.php

--- aula06_01.php
+++ aula06_01.php
@@ -4,12 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Aula06_01</title>
-</head>
+<head>
 <body>
     <h1>Aula06_01 - 11/09/25</h1>
     <form method="post" action="aula06_02.php">
         Login <input type="text" name="login"><br>
-        Senha <input type="password" name="senha"<br>
+        Senha <input type="password" name="senha"><br>
         <input type="submit" value="Entrar">
     </form>
     
