8749b5058e66042c9ad30cc3a342ee6ad4fe9a9a
brunacorreia-gif <brunacorreia@uni9.edu.br>
2025-10-09 08:49:47 -0300

    Atualizar o aula06_01.php

--- aula06_01.php
+++ aula06_01.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Aula06_01</title>
-<head>
+</head>
 <body>
     <h1>Aula06_01 - 11/09/25</h1>
     <form method="post" action="aula06_02.php">
